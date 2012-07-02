e23ca0bd6e71d0b032f8d717e6b51e168030d7d0
Flavio de Sousa <fl@viodesousa.com>
2012-07-02 01:07:52 -0300

    Some dumped data that did not happen to get into RCS VCS, but now getting into git (13 year later!)

--- ChequeOnLine.sql
+++ ChequeOnLine.sql
@@ -49,6 +49,7 @@ CREATE TABLE Cheques (
 #
 
 LOCK TABLES Cheques WRITE;
+INSERT INTO Cheques (associado, cliente, banco, agencia, conta, numero, valor, emissao, vencimento, pago, devolucao_motivo, devolucao_data) VALUES (1,12345678901,123,4567,12341234,5050,100.00,'1999-09-06','1999-09-16',0,NULL,NULL),(1,12345678901,3,123,111,50,250.00,'1999-09-06','1999-10-06',0,NULL,NULL),(1,12345678901,3,123,111,51,118.00,'1999-09-06','1999-10-10',0,NULL,NULL),(1,12345678901,12,444,8090100,888,700.00,'1999-09-06','1999-12-26',0,NULL,NULL),(1,12345678901,123,4567,8901234,7070,380.00,'1999-06-05','1999-07-15',0,7,'1999-07-17'),(1,55365060644,1,1,121,122,200.00,'1999-09-06','1999-10-07',0,NULL,NULL),(1,55365060644,1,1,123,120,150.00,'1999-09-06','1999-09-30',0,NULL,NULL),(1,55365060644,1,125,125,100,150.00,'1999-09-17','1999-10-17',0,NULL,NULL),(1,55365060644,1,2,3,4,1800.00,'1999-09-18','1999-12-18',0,NULL,NULL),(1,55365060644,1,2,5,12,2000.00,'1999-09-18','1999-12-18',0,11,'1999-09-27'),(1,55365060644,128,129,130,140,120.00,'1999-09-23','1999-09-30',0,NULL,NULL),(1,55365060644,129,130,140,150,200.00,'1999-09-23','1999-09-24',0,NULL,NULL),(1,55365060644,10,10,11,128,955.00,'1999-09-26','1999-09-30',0,12,'1999-09-27'),(1,55365060644,11,11,11,11,1500.00,'1999-09-26','1999-09-30',0,11,'1999-09-26'),(1,55365060644,128,129,130,131,10000.00,'1999-09-27','1999-09-28',0,NULL,NULL),(1,55365060644,1,1,1,100,1500.00,'1999-09-27','1999-11-27',0,NULL,NULL),(1,2693763134,12,12,12,12,200.00,'1999-09-27','1999-10-27',0,11,'1999-09-27'),(1,2693763134,12,12,12,13,300.00,'1999-09-27','1999-12-27',0,NULL,NULL),(1,55365060644,1,1,12,10,150.00,'1999-10-02','1999-11-02',0,NULL,NULL),(1,55365060644,1,100,100,11,350.00,'1999-10-02','1999-11-03',0,NULL,NULL);
 UNLOCK TABLES;
 
 #
@@ -64,7 +65,14 @@ CREATE TABLE Clientes (
   cidade varchar(64),
   estado char(2),
   cep varchar(9),
-  fone varchar(64),
+  fone_residencial varchar(64),
+  fone_residencial_recados int(1),
+  fone_comercial varchar(64),
+  trabalho_local varchar(128),
+  trabalho_funcao varchar(64),
+  trabalho_tempo int, 
+  trabalho_salario decimal(14,2),
+  trabalho_outras_rendas decimal(14,2),
   observacoes text,
   PRIMARY KEY (id),
   KEY ClientesPorNome (nome)
@@ -75,6 +83,7 @@ CREATE TABLE Clientes (
 #
 
 LOCK TABLES Clientes WRITE;
+INSERT INTO Clientes (id, nome, fantasia, endereco, bairro, cidade, estado, cep, fone_residencial, observacoes) VALUES (12345678901,'José João da Silva','Zé João','Av Goiás 1234, Apartamento 202','Centro','Anápolis','GO','75123-567','324-1234 até o meio-dia 311-7070 depois','Comentários genéricos,\r\nsem formatação\r\n\r\nPodem\r\nser\r\nescritas\r\ndiversas\r\nlinhas\r\n\r\n...como é comum nesse caso'),(55365060644,'Makário Luiz Orozimbo','','','','Anápolis','GO','75125200','3112533','');
 UNLOCK TABLES;
 
 #
